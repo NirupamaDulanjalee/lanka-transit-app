eb42f2aa5dbeea3a1d25270d0625be74f63c2729
AthulyaWeerakoon <55914834+AthulyaWeerakoon@users.noreply.github.com>
2025-07-23 18:20:21 +0530

    Prebuit android source

---
+++ mobile/transit/android/app/src/main/java/com/transit/wakey/BootCompletedReceiver.java
@@ -0,0 +1,24 @@
+package com.transit.wakey;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Log.d("BootReceiver", "Boot completed. Starting LocationTaskService...");
+
+            Intent serviceIntent = new Intent(context, LocationTaskService.class);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent);
+            } else {
+                context.startService(serviceIntent);
+            }
+        }
+    }
+}
